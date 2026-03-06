64f6c6ea8f0a007ea407d8601dea2b4e73b25a44
ToHoangThanh042004 <Thanhsgk123@gmail.com>
2026-03-06 16:29:01 +0700

    feat: implement forgot password feature

---
+++ apps/web-app/src/modules/auth/index.ts
@@ -0,0 +1,5 @@
+export { default as ForgotPasswordPage } from "./ForgotPasswordPage";
+export { default as CheckEmailPage } from "./CheckEmailPage";
+export { default as VerifyIdentityPage } from "./VerifyIdentityPage";
+export { default as ResetPasswordPage } from "./ResetPasswordPage";
+export { default as PasswordUpdatedPage } from "./PasswordUpdatedPage";
