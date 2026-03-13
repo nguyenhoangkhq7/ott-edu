302fca227c7a77c0b444bf4f73cc5745d31d802e
ToHoangThanh042004 <Thanhsgk123@gmail.com>
2026-03-13 14:41:18 +0700

    Update

--- services/core-service/src/test/java/fit/iuh/CoreServiceApplicationTests.java
+++ services/core-service/src/test/java/fit/iuh/CoreServiceApplicationTests.java
@@ -3,7 +3,14 @@ package fit.iuh;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+   properties = {
+      "spring.autoconfigure.exclude="
+         + "org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration,"
+         + "org.springframework.boot.jpa.autoconfigure.HibernateJpaAutoConfiguration,"
+         + "org.springframework.boot.flyway.autoconfigure.FlywayAutoConfiguration"
+   }
+)
 class CoreServiceApplicationTests {
 
    @Test
