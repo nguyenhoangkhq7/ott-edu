8ec484a1d6854752d42c3bcf8123dddb89abff22
Tran Van Hau <vanhautran592@gmail.com>
2026-04-12 17:56:41 +0700

    update frontend post

--- services/core-service/src/main/java/fit/iuh/modules/auth/repositories/ProfileRepository.java
+++ services/core-service/src/main/java/fit/iuh/modules/auth/repositories/ProfileRepository.java
@@ -5,8 +5,10 @@ import fit.iuh.models.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
     List<Profile> findByAccount_EmailIn(List<String> emails);
+    Optional<Profile> findByAccount_Email(String email);
 
 }
