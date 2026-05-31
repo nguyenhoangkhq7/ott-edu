27e3c22113c8f28fcc143afe8091f0b4c745a136
dongcao0307 <caothanhdong.41118@gmail.com>
2026-05-31 13:01:52 +0700

    add sth

--- services/assignment-service/src/main/java/fit/iuh/modules/quiz/models/Grade.java
+++ services/assignment-service/src/main/java/fit/iuh/modules/quiz/models/Grade.java
@@ -20,6 +20,7 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@com.fasterxml.jackson.annotation.JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Grade {
 
     @Id
